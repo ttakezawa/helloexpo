0a825bef9e62a0ed8d29be881a75d032e6443ccc
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2023-07-14 16:46:16 +0900

    Update tab one

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
 import { StyleSheet } from 'react-native';
-
+import * as Device from 'expo-device'
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Tab One 111</Text>
+      <Text>
+        {Device.manufacturer}: {Device.modelName}
+      </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
